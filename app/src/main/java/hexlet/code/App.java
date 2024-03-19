9db53e9f46c412a9453fe64a660b40424f1c1cac
Alex Belovitskiy <mirrexone@gmail.com>
2024-03-19 14:33:02 +0300

    Added game selection logic

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,31 @@
 package hexlet.code;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+
+        String[] listOfGames = new String[]{"1 - Greet", "0 - Exit"};
+        for (String game : listOfGames) {
+            System.out.println(game);
+        }
+
+        System.out.print("Your choice: ");
+        String userChoice = scanner.next();
+
+        switch (userChoice) {
+            case "1":
+                Cli.greeting();
+                break;
+            case "0":
+                break;
+            default:
+                System.out.println("Choice is outside of the presented");
+        }
+
+        scanner.close();
     }
 }
