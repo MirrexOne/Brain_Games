03fa82f24ee7226b1ea55b19c816a5252aad5d71
Alex Belovitskiy <mirrexone@gmail.com>
2024-03-28 14:18:09 +0300

    Amended launch of prime game

--- app/src/main/java/hexlet/code/interfaces/App.java
+++ app/src/main/java/hexlet/code/interfaces/App.java
@@ -4,6 +4,7 @@ import hexlet.code.games.ArithmeticProgression;
 import hexlet.code.games.Calculator;
 import hexlet.code.games.Even;
 import hexlet.code.games.GreatestCommonDivider;
+import hexlet.code.games.PrimeNumber;
 
 import java.util.Scanner;
 
@@ -37,7 +38,11 @@ public class App {
             case "5":
                 ArithmeticProgression.launchExpressionGame();
                 break;
+            case "6":
+                PrimeNumber.launchEvenGame();
+                break;
             case "0":
+                System.out.println("See you later. Bye.");
                 break;
             default:
                 System.out.println("Nonexistent selection. Please, try again.");
