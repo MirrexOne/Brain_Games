7b2817c450f1d9ea16a7c0d295887d157bcb2533
Alex Belovitskiy <mirrexone@gmail.com>
2024-03-28 15:29:48 +0300

    Change username request in Cli.java

--- app/src/main/java/hexlet/code/interfaces/Cli.java
+++ app/src/main/java/hexlet/code/interfaces/Cli.java
@@ -6,7 +6,7 @@ public class Cli {
     private static final Scanner SCANNER = new Scanner(System.in);
     public static void greeting() {
         System.out.println("\n" + "Welcome to the Brain Games!");
-        System.out.print("What is your name? ");
+        System.out.print("May I have your name? ");
         String userName = SCANNER.nextLine();
         System.out.println("Hello, " + userName + "!");
 
