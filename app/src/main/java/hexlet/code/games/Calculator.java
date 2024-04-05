48c8127c644d918436e5e1a29bcdb4f03ab0d8d2
Alex Belovitskiy <mirrexone@gmail.com>
2024-04-05 10:50:46 +0300

    Refactored Calculator game

--- app/src/main/java/hexlet/code/games/Calculator.java
+++ app/src/main/java/hexlet/code/games/Calculator.java
@@ -7,6 +7,7 @@ public class Calculator {
     private static final String GAME_RULES_DESCRIPTION = "What is the result of the expression?";
     private static final int QUANTITY_OF_QUESTIONS = 3;
     private static final int PAIRS_QUESTION_ANSWER = 2;
+    private static final String[] SET_OF_ACTIONS = {" + ", " - ", " * "};
 
     public static void launchExpressionGame() {
         String[][] gameData = generateGameData();
@@ -19,19 +20,13 @@ public class Calculator {
         for (int i = 0; i < gameData.length; i++) {
             int numberOne = Utils.generateRandomNumber();
             int numberTwo = Utils.generateRandomNumber();
-            String randomAction = generateRandomAction();
+            String randomAction = SET_OF_ACTIONS[Utils.generateRandomNumber(SET_OF_ACTIONS.length)];
             String correctAnswer = String.valueOf(providesCorrectAnswer(randomAction, numberOne, numberTwo));
             gameData[i][0] = numberOne + randomAction + numberTwo;
             gameData[i][1] = correctAnswer;
 
         }
         return gameData;
-    }
-
-    private static String generateRandomAction() {
-        String[] setOfActions = new String[]{" + ", " - ", " * "};
-        int index = Utils.generateRandomNumber(setOfActions.length);
-        return setOfActions[index];
     }
 
     private static int providesCorrectAnswer(String action, int numberOne, int numberTwo) {
