cae6169afb08d2b3a28dac50e7dc02e2dba9755c
Alex Belovitsky <MirrexOne@gamil.com>
2024-04-09 05:41:32 -0400

    Change represent set of games

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -15,9 +15,8 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Please enter the game number and press Enter.");
-        String listOfGames = "1 - Greet ,2 - Even ,3 - Calc ,4 - GCD ,5 - Progression ,6 - Prime ,0 - Exit";
-        System.out.println(listOfGames.replaceAll(",", "\n"));
-
+        String listOfGames = "1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit";
+        System.out.println(listOfGames);
 
         System.out.print("Your choice: ");
         String userChoice = SCANNER.nextLine();
